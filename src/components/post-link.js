20980f14400b8e1dc05bb0f51386ba8c1e68b7e3
mattchupp <mattlchupp@gmail.com>
2019-03-27 12:27:33 -0500

    formatting posts list

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -3,9 +3,12 @@ import { Link } from "gatsby"
 
 const PostLink = ({ post }) => (
   <div>
-    <Link to={post.frontmatter.path}>
-      {post.frontmatter.title} ({post.frontmatter.date})
-    </Link>
+    <h3 style={{margin: '0', padding: '0'}}>
+      <Link style={{textDecoration: 'none'}} to={post.frontmatter.path}>
+        {post.frontmatter.title}
+      </Link>
+    </h3>
+    <p className="meta-text">{post.frontmatter.date}</p>
   </div>
 )
 
