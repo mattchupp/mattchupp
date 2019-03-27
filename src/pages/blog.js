1924a64354d9e1748c3ef7b1a8a2bb7b5a28cfc5
mattchupp <mattlchupp@gmail.com>
2019-03-27 11:04:31 -0500

    navigation between blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,18 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { graphql } from "gatsby"
+import PostLink from "../components/post-link"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Blog Page!</h1>
-    <p>Let's get a listing of those posts</p>
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
-
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  return <Layout>{Posts}</Layout>
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout.js"
+import { Link } from "gatsby"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -9,17 +10,18 @@ export default function Template({
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        {frontmatter.author ? <p>{frontmatter.author}</p> : null}
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h1>{frontmatter.title}</h1>
+          <p className="meta-text">{frontmatter.date}</p>
+          {frontmatter.author ? <p>{frontmatter.author}</p> : null}
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
+      <Link to="/blog">More Posts</Link>
     </Layout>
   )
 }
