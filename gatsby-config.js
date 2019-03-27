99f52997b523afd818a72033ade14e606768880c
mattchupp <mattlchupp@gmail.com>
2019-03-27 10:43:25 -0500

    adding gatsby plugin sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
+    `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
