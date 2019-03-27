271ad8bec21c1276934409c20a602f0db0dea846
mattchupp <mattlchupp@gmail.com>
2019-03-27 15:33:48 -0500

    adding things

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,7 +7,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Portfolio" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>My Work</h1>
+    <h1>Work</h1>
+    <p>Things I do for work</p>
+    <h1>Projects</h1>
+    <p>Things I do for play</p>
   </Layout>
 )
 
