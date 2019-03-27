37012f9f1e8edd0110c1e9699c2414ce523e153d
mattchupp <mattlchupp@gmail.com>
2019-03-27 12:18:15 -0500

    change image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Matt from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hello.</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello. I'm Matt.</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Matt />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
