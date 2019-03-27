f368239832e95b44e184d05212c8248932a3bb93
mattchupp <mattlchupp@gmail.com>
2019-03-27 10:51:16 -0500

    changing some meta data stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `matt-chupp`,
+        short_name: `mattchupp`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
