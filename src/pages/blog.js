c345f326c373a93f7e1fc607d220fb81cfeef0bb
mattchupp <mattlchupp@gmail.com>
2019-03-27 10:54:17 -0500

    creating blog page to list all posts

---
+++ src/pages/blog.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Blog Page!</h1>
+    <p>Let's get a listing of those posts</p>
+
+
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
