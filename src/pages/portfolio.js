15fc06dc0741f75047f6a7baae8a66fc621e6f83
mattchupp <mattlchupp@gmail.com>
2020-05-11 10:45:26 -0500

    updating portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,26 +7,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Portfolio" keywords={[`gatsby`, `portfolio`, `react`]} />
-    <h1>Work</h1>
-    <p>[ Things I do for work ]</p>
-    <Work
-      name="Reverb Conference"
-      postLink="/blog/reverb-conference"
-    />
-    <Work
-      name="More Love More Power"
-      postLink="/blog/"
-    />
-    <Work
-      name="MLMP Info"
-      postLink="/blog/"
-    />
-    <Work
-      name="Hope Center"
-      postLink="/blog/"
-    />
     <h1 style={{paddingTop: '2rem'}}>Projects</h1>
-    <p>[ Things I do for play ]</p>
+    <p>[ Things I do for fun ]</p>
     <Work
       name="Weather"
       postLink="/blog/weather-app"
