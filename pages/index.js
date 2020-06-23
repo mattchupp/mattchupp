52cc1650797f987d8c2a7a0f85512d9b38576ca9
mattchupp <mattlchupp@gmail.com>
2020-06-23 16:22:58 -0500

    page title change

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
+import styled from 'styled-components'
+
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Matt Chupp | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
