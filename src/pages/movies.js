e7ea95c31a4ea2b54c3b3ee564498ae9a514a5a7
mattchupp <mattlchupp@gmail.com>
2020-07-28 08:02:36 -0500

    created object for movies with title & watched boolean

--- src/pages/movies.js
+++ src/pages/movies.js
@@ -2,10 +2,82 @@ import React from "react"
 
 // import Layout from "../components/layout"
 
+const movieList = [
+  {
+    "title": "Godfather",
+    "watched": false
+  },
+  {
+    'title': 'Schindler\'s List',
+    'watched': false
+  },
+  {
+    'title': 'Indiana Jones',
+    'watched': false
+  },
+  {
+    'title': 'Breakfast at Tiffany\'s',
+    'watched': false
+  },
+  {
+    'title': 'Bourne',
+    'watched': false
+  },
+  {
+    'title': 'Goodfellas',
+    'watched': false
+  },
+  {
+    'title': 'James Bond',
+    'watched': false
+  },
+  {
+    'title': 'Airplane',
+    'watched': false
+  },
+  {
+    'title': 'Blade Runner',
+    'watched': false
+  },
+  {
+    'title': 'Saving Private Ryan',
+    'watched': false
+  },
+  {
+    'title': 'Pulp Fiction',
+    'watched': false
+  },
+  {
+    'title': 'Matrix',
+    'watched': false
+  },
+  {
+    'title': 'Fargo',
+    'watched': false
+  },
+  {
+    'title': 'Pans Labyrinth',
+    'watched': false
+  },
+  {
+    'title': 'Shawshank Redemption',
+    'watched': false
+  },
+ 
+  
+]
+
 
 const IndexPage = () => (
   <div>
     <h1>Movie Watchlist</h1>
+
+    {movieList.map(movie => (
+      <div>
+        {movie.title}
+      </div>
+    ))}
+    
   </div>
 )
 
