5b3a781aab9a71f43a645042c876e4b28a03855d
mattchupp <mattlchupp@gmail.com>
2020-07-28 07:49:57 -0500

    added page to list movies I havent watched

---
+++ src/pages/movies.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+// import Layout from "../components/layout"
+
+
+const IndexPage = () => (
+  <div>
+    <h1>Movie Watchlist</h1>
+  </div>
+)
+
+export default IndexPage
