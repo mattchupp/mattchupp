4ed6414cddb26a3ef4e1485067322d013338d90a
mattchupp <mattlchupp@gmail.com>
2020-07-28 09:12:41 -0500

    changed text on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,10 +7,9 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>WHOAAAA SLOW IT DOWN</h1>
+    <h1>UH OH</h1>
     <p>
-    You just hit a route that doesn&#39;t exist... the sadness.
-    That's ok though, we all make mistakes.
+    You just hit a route that doesn&#39;t exist... perhaps the archives are incomplete.
     </p>
     <Link to="/">Go Home</Link>
 
